1a6a1d51741292858b1ba4854bbac687a3c6dddc
MrToy <1659808224@qq.com>
2016-08-21 22:00:06 +0800

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function recur(source,obj,prefix){
 	for(var i in obj){
 		var k=prefix?(prefix+'.'+i):i
-		if(obj[i] instanceof Object){
+		if(obj[i] instanceof Object && (!obj[i][0] || obj[i][0] instanceof Object)){
 			recur(source,obj[i],k)
 		}else{
 			source[k]=obj[i]
